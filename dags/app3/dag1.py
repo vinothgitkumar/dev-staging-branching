9a4420abe7041983ed2c83f9f55ecf1b72ae0163
vinoth <vinoth@gmail.com>
2022-05-25 12:23:43 +0530

    app3 commit01

--- dags/app3/dag1.py
+++ dags/app3/dag1.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.version import version
 from datetime import datetime, timedelta 
-#testing1
+#squash testing1
 
 
 def my_custom_function(ts,**kwargs):
